21ab080f556978341f1f2d5b5706d1507750ba9d
mperezc7 <mperezcalle@uade.edu.ar>
2025-03-26 17:16:41 -0300

    Agregamos listas

---
+++ listas.py
@@ -0,0 +1,10 @@
+frutas = ["manzana", "banana", "cereza", "durazno"]
+print(frutas[0]) # manzana
+print(frutas[2]) # cereza
+print(frutas[-1]) # durazno
+frutas[1] = "naranja"
+print(frutas) # ['manzana', 'naranja', 'cereza', 'durazno']
+
+dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]
+print(dias[-1]) # Viernes
+print(dias[-5]) # Lunes
