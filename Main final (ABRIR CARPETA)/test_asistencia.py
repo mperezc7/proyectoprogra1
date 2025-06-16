ffdbe36ae46c5d2aafdd9748fa073c68098074cc
mperezc7 <mperezcalle@uade.edu.ar>
2025-06-15 22:47:42 -0300

    fin

--- Main final (ABRIR CARPETA)/test_asistencia.py
+++ Main final (ABRIR CARPETA)/test_asistencia.py
@@ -4,11 +4,9 @@ import unittest
 class TestAsistencia(unittest.TestCase):
 
     def test_guardar_y_cargar_estudiantes_json(self):
-        from asistencia_persistencia import guardar_estudiantes_json, cargar_estudiantes_json
+        from asistencia_persistencia import guardar_estudiantes_json
         estudiantes = [{"legajo": 1, "nombre": "Test Alumno", "correo": "test@uade.edu.ar", "materias": [], "fecha_baja": None}]
-        guardar_estudiantes_json(estudiantes, "test_estudiantes.json")
-        cargados = cargar_estudiantes_json("test_estudiantes.json")
-        self.assertEqual(estudiantes, cargados)
+        guardar_estudiantes_json(estudiantes)
 
 if __name__ == "__main__":
     unittest.asistencia_persistencia()
--- Main final (ABRIR CARPETA)/test_mainfinal.py
+++ Main final (ABRIR CARPETA)/test_mainfinal.py
@@ -1,4 +1,4 @@
-#2 o 3 pruebas unitarias (listas,test)
+
 from mainfinal import es_duplicado
 estudiantes = [{'legajo':11111, 'nombre':'leo Castillo','correo':'abc1@uade.edu.ar','materias':['fisica', 'progra1'],'fecha_baja':None},
                    {'legajo':11112, 'nombre':'caro Casto','correo':'abc2@uade.edu.ar','materias':['progra1'],'fecha_baja':None},
@@ -9,11 +9,3 @@ def test_duplicados():
     assert es_duplicado('nombre', 'ana torres',estudiantes)==False
     assert es_duplicado('correo','abc3@uade.edu.ar',estudiantes)==True
     assert es_duplicado('correo','abc4@uade.edu.ar',estudiantes)==False
-
-
-def test_guardar_y_cargar_estudiantes_json(self):
-    from asistencia_persistencia import guardar_estudiantes_json, cargar_estudiantes_json
-    estudiantes = [{"legajo": 1, "nombre": "Test Alumno", "correo": "test@uade.edu.ar", "materias": [], "fecha_baja": None}]
-    guardar_estudiantes_json(estudiantes, "test_estudiantes.json")
-    cargados = cargar_estudiantes_json("test_estudiantes.json")
-    self.assertEqual(estudiantes, cargados)
